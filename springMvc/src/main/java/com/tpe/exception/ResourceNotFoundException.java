2a63121e9f8ba104acb5538040735dda19aa4e53
Omer Isik <isikomer4545@gmail.com>
2023-01-29 19:12:24 +0100

    Service Layer is done

---
+++ springMvc/src/main/java/com/tpe/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tpe.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+}
---
+++ springMvc/src/main/java/com/tpe/service/StudentServiceImpl.java
@@ -0,0 +1,44 @@
+package com.tpe.service;
+
+import com.tpe.domain.Student;
+import com.tpe.exception.ResourceNotFoundException;
+import com.tpe.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service // this handle the @Component inside this annotation does something more and same as @Component
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.getAll();
+    }
+
+    @Override
+    public Student findStudentById(Long id) {
+        //Optional<Student> student = studentRepository.findById(id);// it can be done like this, but we will improve it below
+        Student student= studentRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Student is not found with id: " + id));
+        return student;
+    }
+
+    @Override
+    public void saveStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+        studentRepository.update(student);
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepository.delete(id);
+    }
+}
