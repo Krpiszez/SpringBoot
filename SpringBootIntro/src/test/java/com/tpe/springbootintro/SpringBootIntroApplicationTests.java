d8fd9403a62d4b1113d6f7226f8018308e574a87
Omer Isik <isikomer4545@gmail.com>
2023-02-02 17:59:09 +0100

    intro to Spring Boot

---
+++ SpringBootIntro/src/test/java/com/tpe/springbootintro/SpringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpe.springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
