3d5347d17f1a4a4e97ed0fc8feef518f41478062
Omer Isik <isikomer4545@gmail.com>
2023-02-11 18:02:27 +0100

    3 Layers

---
+++ ecommerce/src/main/java/com/tpe/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package com.tpe.controller;
+
+import com.tpe.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+}
---
+++ ecommerce/src/main/java/com/tpe/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.tpe.service;
+
+import com.tpe.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryRepository categoryRepository;
+}
