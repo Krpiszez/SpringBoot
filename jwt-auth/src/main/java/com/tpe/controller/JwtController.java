5a9a8b398a7d62253f10a75b85bf5673dc05199a
Omer Isik <isikomer4545@gmail.com>
2023-02-19 16:12:11 +0100

    User Service added.

--- jwt-auth/src/main/java/com/tpe/controller/JwtController.java
+++ jwt-auth/src/main/java/com/tpe/controller/JwtController.java
@@ -1,19 +1,27 @@
 package com.tpe.controller;
 
+import com.tpe.dto.RegisterRequest;
+import com.tpe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping
 public class JwtController {
 
-//    @PostMapping("/register")
-//    public ResponseEntity<String> registerUser(@RequestBody ){
-//
-//    }
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody RegisterRequest registerRequest){
+
+    }
 
 
 }
---
+++ jwt-auth/src/main/java/com/tpe/service/UserService.java
@@ -0,0 +1,10 @@
+package com.tpe.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+
+}
